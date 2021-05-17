d0377fd0cea6b12469e0de4aa0d63dc4ea659140
Tomasz Krutowski <tkrutowski@gmail.com>
2021-05-17 22:39:17 +0200

    v1 - test

---
+++ src/main/java/net/focik/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,14 @@
+package net.focik.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/focik/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.focik.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
